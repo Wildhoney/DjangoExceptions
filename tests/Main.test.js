20d2b65bd2a34ba87a9df1fcdb43ee4b4c022b9f
Wildhoney <adam.timberlake@gmail.com>
2017-06-20 17:48:47 +0100

    Updated tests to assert deepEqual

--- tests/Main.test.js
+++ tests/Main.test.js
@@ -1,6 +1,16 @@
 import test from 'ava';
 import { parse } from '../dist/main';
 
-test('...', t => {
-    t.is(parse('Hello Adam'), 'HELLO ADAM');
+test('It should be able to parse a simple non-nested validation structure;', t => {
+
+    const messages = {
+        'name': ['Must be at least 16 characters.'],
+        'email': ['Must be at least 32 characters.', 'Must be a valid e-mail address.']
+    };
+
+    t.deepEqual(parse(messages), [
+        { field: ['name'], messages: ['Must be at least 16 characters.' ]},
+        { field: ['email'], messages: ['Must be at least 32 characters.', 'Must be a valid e-mail address.' ]}
+    ]);
+
 });
