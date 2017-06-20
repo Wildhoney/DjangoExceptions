7a51a5e8583f90a13b24e505cc408d8ebdb59d0d
Wildhoney <adam.timberlake@gmail.com>
2017-06-20 17:47:38 +0100

    Added initial parsing

--- src/purs/Exceptions.purs
+++ src/purs/Exceptions.purs
@@ -1,6 +1,13 @@
-module Exceptions (parse) where
+module Exceptions (parse, Field) where
 
-import Data.String (toUpper)
+import Prelude
+import Data.Maybe as M
+import Data.StrMap as SM
 
-parse ∷ String -> String
-parse x = toUpper x
+newtype Field = Field { field :: Array String, messages :: Array String }
+
+read ∷ ∀ m. String -> SM.StrMap (Array m) -> Array m
+read field model = M.fromMaybe [] $ SM.lookup field model
+
+parse ∷ SM.StrMap (Array String) -> Array Field
+parse model =  map (\field -> Field { field: [field], messages: read field model }) $ SM.keys model
