8fc0e25ff002ffc71f04da0f397b6b1962ee93fc
Wildhoney <adam.timberlake@gmail.com>
2017-06-26 20:44:04 +0100

    Added initial working example

--- src/DjangoExceptions.js
+++ src/DjangoExceptions.js
@@ -1,1 +1,20 @@
-export * from './purs/Exceptions.purs';
+import m from 'moggy';
+
+/**
+ * @method parse
+ * @param {Object} input
+ * @param {Array} group
+ * @return {Array} 
+ */
+export const parse = (input, group = []) => {
+
+    return Object.keys(input).reduce((xs, key) => {
+
+        const isGroup = typeof input[key] === 'object' && !Array.isArray(input[key]);
+        const model = { field: group.concat(key), messages: input[key] };
+
+        return isGroup ? xs.concat(parse({ ...input[key] }, group.concat(key))) : [...xs, model];
+
+    }, []);
+
+};
--- tests/DjangoExceptions.test.js
+++ tests/DjangoExceptions.test.js
@@ -1,5 +1,5 @@
 import test from 'ava';
-import { parse } from '../dist/main';
+import { parse } from '../src/DjangoExceptions';
 
 test('It should be able to parse a simple non-nested validation structure;', t => {
 
@@ -8,12 +8,10 @@ test('It should be able to parse a simple non-nested validation structure;', t =
         'email': ['Must be at least 32 characters.', 'Must be a valid e-mail address.']
     };
 
-    console.log(parse(messages));
-
-    // t.deepEqual(parse(messages), [
-    //     { field: ['name'], messages: ['Must be at least 16 characters.' ]},
-    //     { field: ['email'], messages: ['Must be at least 32 characters.', 'Must be a valid e-mail address.' ]}
-    // ]);
+    t.deepEqual(parse(messages), [
+        { field: ['name'], messages: ['Must be at least 16 characters.' ]},
+        { field: ['email'], messages: ['Must be at least 32 characters.', 'Must be a valid e-mail address.' ]}
+    ]);
 
 });
 
@@ -21,21 +19,40 @@ test('It should be able to parse a semi-complex nested validation structure;', t
 
     const messages = {
         person: {
-            'firstName': ['Must be at least 16 characters.'],
+            'name': ['Must be at least 16 characters.'],
             'email': ['Must be at least 32 characters.', 'Must be a valid e-mail address.']
         }
     };
 
-    // t.deepEqual(parse(messages), [
-    //     { field: ['person', 'name'], messages: ['Must be at least 16 characters.' ]},
-    //     { field: ['person', 'email'], messages: ['Must be at least 32 characters.', 'Must be a valid e-mail address.' ]}
-    // ]);
+    t.deepEqual(parse(messages), [
+        { field: ['person', 'name'], messages: ['Must be at least 16 characters.' ]},
+        { field: ['person', 'email'], messages: ['Must be at least 32 characters.', 'Must be a valid e-mail address.' ]}
+    ]);
 
-    console.log(parse(messages)([]));
+});
 
-    // t.deepEqual(parse(messages), [
-    //     { field: ['name'], messages: ['Must be at least 16 characters.' ]},
-    //     { field: ['email'], messages: ['Must be at least 32 characters.', 'Must be a valid e-mail address.' ]}
-    // ]);
+test('It should be able to parse a complex nested validation structure;', t => {
+
+    const messages = {
+        person: {
+            'name': {
+                'firstName': ['Must be at least 16 characters.'],
+                'lastName': ['Must be at least 32 characters.']
+            },
+            'email': ['Must be at least 32 characters.', 'Must be a valid e-mail address.']
+        },
+        address: {
+            'city': ['This field is required.']
+        },
+        'personId': ['This field is required.']
+    };
+
+    t.deepEqual(parse(messages), [
+        { field: ['person', 'name', 'firstName'], messages: ['Must be at least 16 characters.' ]},
+        { field: ['person', 'name', 'lastName'], messages: ['Must be at least 32 characters.' ]},
+        { field: ['person', 'email'], messages: ['Must be at least 32 characters.', 'Must be a valid e-mail address.' ]},
+        { field: ['address', 'city'], messages: ['This field is required.' ]},
+        { field: ['personId'], messages: ['This field is required.'] }
+    ]);
 
 });
